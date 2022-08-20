58c831113443cfd6c3909019b471fdea28403fe8
Xurshidjon Yunusov <64409596+yxurshidjon@users.noreply.github.com>
2022-08-20 14:04:20 +0500

    Add files via upload

---
+++ Xurshidjon.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Xurshid.Com</title>
+</head>
+<body>
+
+	<?php 
+
+echo "Salom Dunyo"; // string
+
+echo "<br>";
+
+print("Salom Dunyo"); // arg
+
+echo "<br>";
+
+printf("Salom Dunyo"); // format
+
+echo "<br>";
+
+print_r("Salom Dunyo"); // expression
+
+	 ?>
+	
+</body>
+</html>
